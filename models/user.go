c82526ef4ed9e047fbc16d69ecbc584bbd22724c
SteveMCWin <stevp003@gmail.com>
2025-09-09 17:46:56 +0200

    adding more stuff to user

--- models/user.go
+++ models/user.go
@@ -1,8 +1,17 @@
 package models
 
+import "time"
+
 type User struct {
-	Id uint64
-	UserName string
-	Password string
-	// other stats
+	Id          uint64
+	UserName    string
+	Password    string
+	Email       string
+	DateCreated time.Time
+
+	// stats
+	TestsStarted   uint
+	TestsCompleted uint
+	AllTimeAvgWPM  float32
+	AllTimeAvgACC  float32
 }
